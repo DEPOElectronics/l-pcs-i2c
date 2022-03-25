6cb84a68a99f5d312f1b851413720b600b2a5115
Glukhov Mikhail <mikl@greenfil.ru>
2022-03-25 16:48:40 +0300

    Добавил список адресов + мелкие правки

--- l-pcs-i2c.c
+++ l-pcs-i2c.c
@@ -3,6 +3,15 @@
 #include <linux/hwmon.h>
 #include <linux/hwmon-sysfs.h>
 
+/* Addresses to scan */
+static const unsigned short normal_i2c[] = {
+	0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x25, 
+	0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x35, 
+	0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x45, 
+	0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x55, 
+	I2C_CLIENT_END };
+
+
 struct l_pcs_i2c_data
 {
     struct i2c_client   *client;
@@ -14,12 +23,14 @@ struct l_pcs_i2c_data
 
 static void l_pcs_i2c_update_device(struct device *dev)
 {
+    const int DataReadCount=2;
+    
     struct l_pcs_i2c_data *data = dev_get_drvdata(dev);
     mutex_lock(&data->lock);
     if (time_after(jiffies, data->last_updated + HZ + HZ / 2) || !data->valid)
     {
         char bytes[2];
-        if (i2c_master_recv(data->client, bytes, 2) != 2)
+        if (i2c_master_recv(data->client, bytes, DataReadCount) != DataReadCount)
         {
             data->temp = 0;
             data->valid = 0;
@@ -52,7 +63,9 @@ static int l_pcs_i2c_probe(struct i2c_client *client)
     struct device *dev = &client->dev;
     struct l_pcs_i2c_data *data;
     struct device *hwmon_dev;
-    if((data = devm_kzalloc(dev, sizeof(struct l_pcs_i2c_data), GFP_KERNEL)) == NULL) return -ENOMEM;
+    data = devm_kzalloc(dev, sizeof(struct l_pcs_i2c_data), GFP_KERNEL);
+    if (!data) 
+    	return -ENOMEM;
     i2c_set_clientdata(client, data);
     mutex_init(&data->lock);
     data->client = client;
@@ -65,13 +78,15 @@ MODULE_DEVICE_TABLE(i2c, l_pcs_i2c_ids);
 
 static struct i2c_driver l_pcs_i2c_driver =
 {
-    .driver         = { .name = KBUILD_MODNAME },
-    .probe_new      = l_pcs_i2c_probe,
-    .id_table       = l_pcs_i2c_ids,
+	.class	= I2C_CLASS_HWMON,
+	.driver	= { .name = KBUILD_MODNAME },
+	.probe_new	= l_pcs_i2c_probe,
+	.id_table	= l_pcs_i2c_ids,
+	.address_list   = normal_i2c,
 };
 module_i2c_driver(l_pcs_i2c_driver);
 
 MODULE_AUTHOR("Igor Molchanov <akemi_homura@kurisa.ch>");
 MODULE_DESCRIPTION("Elbrus PCS I2C access hwmon module");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0");
+MODULE_VERSION("1.1");
